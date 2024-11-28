bedf983ac4ae34f8fa3ce382b185a6c94a11f15f
altie122 <77561831+altie122@users.noreply.github.com>
2024-11-27 17:22:13 -0700

    fix a link and the sitemap (on prod)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,10 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+const site = "https://v4.bcuw.xyz";
 // https://astro.build/config
 export default defineConfig({
+  site,
   integrations: [
     tailwind({
       applyBaseStyles: false,

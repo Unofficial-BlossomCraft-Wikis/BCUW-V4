83f0def042dbbdb7bb3384135b2aa917f7142888
altie122 <altielynn@gmail.com>
2025-01-11 20:40:36 -0700

    remove unused file

--- client/website/src/components/SwaggerUIWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css";
-
-const SwaggerUIWrapper = () => {
-  return <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />;
-};
-
-export default SwaggerUIWrapper;

784d721d879000bf4bbb322f2d082f173aa2f7ab
altie122 <altielynn@gmail.com>
2025-01-11 21:43:50 -0700

    change how most depends are done | make api dir

---
+++ backend/api/astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    mdx(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  server: { port: 3000 },
+});
